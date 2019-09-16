b1353e37d8c54c8e3476336ab0fab5df7b56b77f
Stefan Huber <stefan.huber.niedling@outlook.com>
2019-09-16 18:41:21 +0200

    Call generation from within getTest()

--- Test.py
+++ Test.py
@@ -1,4 +1,7 @@
 from pathlib import Path
 
+
 def getTest():
+    from TrackGeneration import GenerateTrack
+    GenerateTrack(25, 3, False, False)
     return (Path("track_0.xml"), Path("criteria.xml"))
